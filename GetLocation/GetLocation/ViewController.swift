b26a6a29ea73d06cac353159a59f8c03b84deb66
Kazuya <shiozaki5621@gmail.com>
2019-06-17 14:56:32 -0700

    add extention for dividing view and delegate

--- GetLocation/GetLocation/ViewController.swift
+++ GetLocation/GetLocation/ViewController.swift
@@ -12,26 +12,26 @@ import UIKit
 // 1. import CoreLocation FW
 import CoreLocation
 
-class ViewController: UIViewController ,CLLocationManagerDelegate {
-
+class ViewController: UIViewController {
+  
   // 2-1. To declare locationManager instance as a class member
   var locationManager: CLLocationManager!
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setupLocationManager()
   }
-
-
+  
+  
   // 2-2. Set upper for locationManager
   func setupLocationManager() {
     locationManager = CLLocationManager()
-
+    
     // 3-1. To request grant for get location
     // 3-2. To request the grant when success to create location manager
     guard let locationManager = locationManager else {return}
     locationManager.requestWhenInUseAuthorization()
-
+    
     // 4-1. Setting Manager when success to get grant for location
     // 4-2. To get grant status from "CLLOcationManager" class
     let status = CLLocationManager.authorizationStatus()
@@ -39,24 +39,13 @@ class ViewController: UIViewController ,CLLocationManagerDelegate {
     if status == .authorizedWhenInUse {
       // 5-2. To set delegate when status is authorizedWhenInUse
       locationManager.delegate = self
-
+      
       // unit is meter
       locationManager.distanceFilter = 10
       locationManager.startUpdatingLocation()
     }
   }
-
-  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    let location = locations.first
-    let latitude = location?.coordinate.latitude
-    let longitude = location?.coordinate.longitude
-
-    print("latitude: \(latitude!) \nlogntitude \(longitude!)")
-
-  }}
-
-// 5-1. To extend ViewController by CLLocationManagerDelegate
-//extension ViewController: CLLocationManagerDelegate {
+  
 //  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 //    let location = locations.first
 //    let latitude = location?.coordinate.latitude
@@ -65,5 +54,17 @@ class ViewController: UIViewController ,CLLocationManagerDelegate {
 //    print("latitude: \(latitude!) \nlogntitude \(longitude!)")
 //
 //  }
-//}
+  
+}
+
+// 5-1. To extend ViewController by CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location = locations.first
+    let latitude = location?.coordinate.latitude
+    let longitude = location?.coordinate.longitude
+
+    print("latitude: \(latitude!) \nlogntitude \(longitude!)")
+  }
+}
 

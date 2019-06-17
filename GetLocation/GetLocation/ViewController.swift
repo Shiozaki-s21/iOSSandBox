4a73ab6d3ff167d20526d8682cc7f8404fb6b419
Kazuya <shiozaki5621@gmail.com>
2019-06-17 03:36:42 -0700

    add function for getting location

---
+++ GetLocation/GetLocation/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  GetLocation
+// Sand box for get Location date
+//
+//
+//  Created by SubaruShiozaki on 2019-06-16.
+//  Copyright © 2019 Kazuya Takahashi. All rights reserved.
+//
+
+import UIKit
+// 1. import CoreLocation FW
+import CoreLocation
+
+class ViewController: UIViewController ,CLLocationManagerDelegate {
+
+  // 2-1. To declare locationManager instance as a class member
+  var locationManager: CLLocationManager!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupLocationManager()
+  }
+
+
+  // 2-2. Set upper for locationManager
+  func setupLocationManager() {
+    locationManager = CLLocationManager()
+
+    // 3-1. To request grant for get location
+    // 3-2. To request the grant when success to create location manager
+    guard let locationManager = locationManager else {return}
+    locationManager.requestWhenInUseAuthorization()
+
+    // 4-1. Setting Manager when success to get grant for location
+    // 4-2. To get grant status from "CLLOcationManager" class
+    let status = CLLocationManager.authorizationStatus()
+    // Validation status
+    if status == .authorizedWhenInUse {
+      // 5-2. To set delegate when status is authorizedWhenInUse
+      locationManager.delegate = self
+
+      // unit is meter
+      locationManager.distanceFilter = 10
+      locationManager.startUpdatingLocation()
+    }
+  }
+
+  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location = locations.first
+    let latitude = location?.coordinate.latitude
+    let longitude = location?.coordinate.longitude
+
+    print("latitude: \(latitude!) \nlogntitude \(longitude!)")
+
+  }}
+
+// 5-1. To extend ViewController by CLLocationManagerDelegate
+//extension ViewController: CLLocationManagerDelegate {
+//  public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//    let location = locations.first
+//    let latitude = location?.coordinate.latitude
+//    let longitude = location?.coordinate.longitude
+//
+//    print("latitude: \(latitude!) \nlogntitude \(longitude!)")
+//
+//  }
+//}
+

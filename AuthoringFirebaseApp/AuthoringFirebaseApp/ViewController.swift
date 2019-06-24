f647886d6115e24c9a4bed3402a8318094bd51ce
Kazuya <shiozaki5621@gmail.com>
2019-06-23 18:06:54 -0700

    add Authoring with Firebase

---
+++ AuthoringFirebaseApp/AuthoringFirebaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthoringFirebaseApp
+//
+//  Created by SubaruShiozaki on 2019-06-23.
+//  Copyright © 2019 Kazuya Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

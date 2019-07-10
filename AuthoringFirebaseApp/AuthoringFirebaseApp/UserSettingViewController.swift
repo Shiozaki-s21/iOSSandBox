7ce6ed2465421584ce880aa80c11242d80907e79
Kazuya <shiozaki5621@gmail.com>
2019-07-09 23:02:47 -0700

    add user authoring

---
+++ AuthoringFirebaseApp/AuthoringFirebaseApp/UserSettingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UserSettingViewController.swift
+//  AuthoringFirebaseApp
+//
+//  Created by SubaruShiozaki on 2019-07-09.
+//  Copyright © 2019 Kazuya Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //TODO To create user setting screen
+
+    //TODO it must be had picture, bio, userName
+    
+}

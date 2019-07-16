1d5a289aa40232412be9c873579d3688f766363c
Kazuya <shiozaki5621@gmail.com>
2019-07-16 16:28:36 -0700

    add photo upload function

--- AuthoringFirebaseApp/AuthoringFirebaseApp/UserSettingViewController.swift
+++ AuthoringFirebaseApp/AuthoringFirebaseApp/UserSettingViewController.swift
@@ -7,34 +7,73 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
+
 
 class UserSettingViewController: UIViewController {
-
+  
   // user name label which is editable
-
+  
   // user profile picture
-
+  
   // user bio
-
+  
   // submit button(if auto save doesn't work)
-
-  let nameLabel: UILabel = UILabel()
-
   var userModel: UserModel?
-
+  
+  let nameLabel: UITextField = UITextField()
+  let uidTextField:UITextField = UITextField()
+  @IBOutlet var userPhoto: UIButton!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.addSubview(nameLabel)
     nameLabel.translatesAutoresizingMaskIntoConstraints = false
-    nameLabel.text = "false"
     nameLabel.text = userModel?.userName
     nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-
+    
+    //    userPhoto.translatesAutoresizingMaskIntoConstraints = false
+    
+    
+    
+    
   }
+  
+  @IBAction func uploadPhoto(_ sender: UIButton) {
+    
+    
+  }
+}
 
-  //TODO To create user setting screen
+extension UserSettingViewController:UIImagePickerControllerDelegate {
+  
+  public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+    let storage = Storage.storage()
+    let storageRef = storage.reference(forURL: "gs://authoringtest-8a6bc.appspot.com/")
+    
+    if let data = UIImagePNGRepresentation(info[UIImagePickerControllerOriginalImage] as! UIImage) {
+      let reference = storageRef.child("image/" + NSUUID().uuidString + "/" + countPhoto() + ".jpg")
+      reference.put(data, metadata: nil, completion: { metaData, error in
+        print(metaData)
+        print(error)
+      })
+      dismiss(animated: true, completion: nil)
+    }
+    
+  }
+}
 
-  //TODO it must be had picture, bio, userName
-
+extension UserSettingViewController:UINavigationControllerDelegate {
+  func pickImageFromLibrary() {
+    if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+      let controller = UIImagePickerController()
+      controller.delegate = self
+      controller.sourceType = .photoLibrary
+      
+      present(controller, animated: true, completion: nil)
+    }
+  }
 }

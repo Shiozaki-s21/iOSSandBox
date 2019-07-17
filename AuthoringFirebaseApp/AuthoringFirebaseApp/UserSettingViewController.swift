312d70b0737a53af4f0eb73532554d36262c2607
Kazuya <shiozaki5621@gmail.com>
2019-07-17 15:48:00 -0700

    add photo picker

--- AuthoringFirebaseApp/AuthoringFirebaseApp/UserSettingViewController.swift
+++ AuthoringFirebaseApp/AuthoringFirebaseApp/UserSettingViewController.swift
@@ -12,21 +12,13 @@ import FirebaseCore
 
 
 class UserSettingViewController: UIViewController {
-  
-  // user name label which is editable
-  
-  // user profile picture
-  
-  // user bio
-  
-  // submit button(if auto save doesn't work)
+
   var userModel: UserModel?
   
   let nameLabel: UITextField = UITextField()
   let uidTextField:UITextField = UITextField()
   @IBOutlet var userPhoto: UIButton!
-  
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     view.addSubview(nameLabel)
@@ -36,33 +28,21 @@ class UserSettingViewController: UIViewController {
     nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     
     //    userPhoto.translatesAutoresizingMaskIntoConstraints = false
-    
-    
-    
-    
   }
   
   @IBAction func uploadPhoto(_ sender: UIButton) {
-    
-    
+    print("upload button")
+    pickImageFromLibrary()
   }
 }
 
 extension UserSettingViewController:UIImagePickerControllerDelegate {
   
   public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-    let storage = Storage.storage()
-    let storageRef = storage.reference(forURL: "gs://authoringtest-8a6bc.appspot.com/")
-    
-    if let data = UIImagePNGRepresentation(info[UIImagePickerControllerOriginalImage] as! UIImage) {
-      let reference = storageRef.child("image/" + NSUUID().uuidString + "/" + countPhoto() + ".jpg")
-      reference.put(data, metadata: nil, completion: { metaData, error in
-        print(metaData)
-        print(error)
-      })
-      dismiss(animated: true, completion: nil)
-    }
-    
+    let storage = Storage.storage().reference()
+    let imageSelf = storage.child("userProfImage")
+//    let storageRef = storage.reference(forURL: "gs://authoringtest-8a6bc.appspot.com/")
+    print("Imagepicker")
   }
 }
 
